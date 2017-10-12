bcb54c15583b661b4381fabec25691ffc823549a
Kamesuta <kamesuta@gmail.com>
2017-10-12 21:56:07 +0900

    #

--- src/main/java/net/teamfruit/usefulbuilderswand/WandData.java
+++ src/main/java/net/teamfruit/usefulbuilderswand/WandData.java
@@ -1,39 +1,27 @@
 package net.teamfruit.usefulbuilderswand;
 
-import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import net.teamfruit.usefulbuilderswand.ItemLore.ItemLoreDataFormat;
-
 public class WandData {
 	public static final String SETTING_EFFECT_RANGE = "setting.effect.range";
 
-	public static final String META_PREFIX = "itemdata.prefix";
-	public static final String META_VALUE_PREFIX = "itemdata.value.prefix";
-	public static final String META_VALUE_SUFFIX = "itemdata.value.suffix";
-	public static final String META_FORMAT = "itemdata.format";
-
-	public static final String FEATURE = "feature";
-	public static final String FEATURE_META = "feature.meta";
 	public static final String FEATURE_META_SIZE = "feature.meta.size";
 	public static final String FEATURE_META_MODE = "feature.meta.mode";
-	// public static final String FEATURE_META_EXP = "feature.meta.exp.data";
-	// public static final String FEATURE_META_EXP_MAX = "feature.meta.exp.max";
 	public static final String FEATURE_META_DURABILITY = "feature.meta.durability.data";
 	public static final String FEATURE_META_DURABILITY_MAX = "feature.meta.durability.max";
+	public static final String FEATURE_META_DURABILITY_BLOCKCOUNT = "feature.meta.durability.blockcount";
 	public static final String FEATURE_META_DURABILITY_UNBREAKABLE = "feature.meta.durability.unbreakable";
 	public static final String FEATURE_META_COUNT_PLACE = "feature.meta.count.place";
 	public static final String FEATURE_META_COUNT_USE = "feature.meta.count.use";
-	// public static final String FEATURE_META_PARTICLE = "feature.meta.particle";
 	public static final String FEATURE_META_PARTICLE_COLOR_R = "feature.meta.particle.color.r";
 	public static final String FEATURE_META_PARTICLE_COLOR_G = "feature.meta.particle.color.g";
 	public static final String FEATURE_META_PARTICLE_COLOR_B = "feature.meta.particle.color.b";
@@ -41,8 +29,6 @@ public class WandData {
 	public static final String FEATURE_META_OWNER = "feature.meta.owner.data";
 	public static final String FEATURE_META_OWNER_ID = "feature.meta.owner.id";
 	public static final String FEATURE_META_OWNER_NAME = "feature.meta.owner.name";
-	// public static final String FEATURE_DISPLAY_LEVEL = "feature.display.level";
-	// public static final String FEATURE_DISPLAY_PARTICLE = "feature.display.particle";
 
 	private FileConfiguration cfg;
 
@@ -54,81 +40,96 @@ public class WandData {
 
 		cfg.addDefault(SETTING_EFFECT_RANGE, 48);
 
-		cfg.addDefault(META_PREFIX, "§3§e§8§r");
-		cfg.addDefault(META_VALUE_PREFIX, "§5§2§c§r");
-		cfg.addDefault(META_VALUE_SUFFIX, "§a§6§3§r");
+		final Map<String, Object> it = Maps.newHashMap();
 
-		final Map<String, Object> features = Maps.newHashMap();
-		features.put(FEATURE_META_SIZE, "§2§f§c§r");
-		features.put(FEATURE_META_MODE, "§1§d§3§r");
-		// features.put(FEATURE_META_EXP, "§e§9§8§r");
-		// features.put(FEATURE_META_EXP_MAX, "§d§3§8§r");
-		features.put(FEATURE_META_DURABILITY, "§5§b§4§r");
-		features.put(FEATURE_META_DURABILITY_MAX, "§7§f§c§r");
-		features.put(FEATURE_META_DURABILITY_UNBREAKABLE, "§3§a§c§r");
-		features.put(FEATURE_META_COUNT_PLACE, "§6§8§e§r");
-		features.put(FEATURE_META_COUNT_USE, "§2§7§b§r");
-		// features.put(FEATURE_META_PARTICLE, "§3§4§b§r");
-		features.put(FEATURE_META_PARTICLE_COLOR_R, "§4§3§4§r");
-		features.put(FEATURE_META_PARTICLE_COLOR_G, "§c§a§d§r");
-		features.put(FEATURE_META_PARTICLE_COLOR_B, "§f§8§e§r");
-		features.put(FEATURE_META_PARTICLE_SHARE, "§a§f§4§r");
-		features.put(FEATURE_META_OWNER, "§e§6§d§r");
-		features.put(FEATURE_META_OWNER_ID, "§d§4§2§r");
-		features.put(FEATURE_META_OWNER_NAME, "§6§c§8§r");
-		// features.put(FEATURE_DISPLAY_LEVEL, "§2§a§b§r");
-		// features.put(FEATURE_DISPLAY_PARTICLE, "§4§4§f§r");
+		it.put(FEATURE_META_SIZE, 9);
+		it.put(FEATURE_META_MODE, false);
+		it.put(FEATURE_META_DURABILITY, 27);
+		it.put(FEATURE_META_DURABILITY_MAX, 27);
+		it.put(FEATURE_META_DURABILITY_BLOCKCOUNT, false);
+		it.put(FEATURE_META_DURABILITY_UNBREAKABLE, false);
+		it.put(FEATURE_META_COUNT_PLACE, 0);
+		it.put(FEATURE_META_COUNT_USE, 0);
+		it.put(FEATURE_META_PARTICLE_COLOR_R, 255);
+		it.put(FEATURE_META_PARTICLE_COLOR_G, 255);
+		it.put(FEATURE_META_PARTICLE_COLOR_B, 255);
+		it.put(FEATURE_META_PARTICLE_SHARE, true);
+		it.put(FEATURE_META_OWNER, false);
+		it.put(FEATURE_META_OWNER_ID, "");
+		it.put(FEATURE_META_OWNER_NAME, "");
 
-		final List<String> format = Lists.newArrayList(new String[] {
-				"§eBuilder's Wand §7x${i:"+ft(FEATURE_META_SIZE)+"=0} §7[${b:"+ft(FEATURE_META_MODE)+"=┃:━}§7] (${b:"+ft(FEATURE_META_DURABILITY_UNBREAKABLE)+"=Infinity:${i:"+ft(FEATURE_META_DURABILITY)+"=0}/${i:"+ft(FEATURE_META_DURABILITY_MAX)+"=0}}§7)",
-				"§3 - Mode §7: ${B:"+ft(FEATURE_META_MODE)+"=Vertical:Horizonal}",
-				"§3 - Durability §7: ${i:"+ft(FEATURE_META_DURABILITY)+"=§0}${I:"+ft(FEATURE_META_DURABILITY_MAX)+"=§0}${B:"+ft(FEATURE_META_DURABILITY_UNBREAKABLE)+"=(Infinity):${i:"+ft(FEATURE_META_DURABILITY)+"=0} of ${i:"+ft(FEATURE_META_DURABILITY_MAX)+"=0}}",
-				"§3 - Size §7: ${I:"+ft(FEATURE_META_SIZE)+"=0}${I:"+ft(FEATURE_META_PARTICLE_COLOR_R)+"=§255}${I:"+ft(FEATURE_META_PARTICLE_COLOR_G)+"=§255}${I:"+ft(FEATURE_META_PARTICLE_COLOR_B)+"=§255}${B:"+ft(FEATURE_META_PARTICLE_SHARE)+"=true}",
-				"§3 - UseCount §7: ${I:"+ft(FEATURE_META_COUNT_USE)+"=0}",
-				"§3 - PlaceCount §7: ${I:"+ft(FEATURE_META_COUNT_PLACE)+"=0}",
-				"${B:"+ft(FEATURE_META_OWNER)+"=§3 - Owner §7： ${S:"+ft(FEATURE_META_OWNER_NAME)+"=}${I:"+ft(FEATURE_META_OWNER_ID)+"=§}:}",
-				// "§3 - Level §7: ${I:"+ft(FEATURE_DISPLAY_LEVEL)+"=0}",
-				// "§e - Exp : ${I:"+ft(FEATURE_META_EXP+")=0} of ${I:"+ft(FEATURE_META_EXP_MAX)+"=0}",
-				// "§e - Particle   : ${I:"+ft(FEATURE_META_PARTICLE)+"=§}${S:"+ft(FEATURE_DISPLAY_PARTICLE)+"}",
-		});
+		it.put("custom.title.mode.if", "${"+FEATURE_META_MODE+"}");
+		it.put("custom.title.mode.true", "┃");
+		it.put("custom.title.mode.false", "━");
+		it.put("custom.title.durability.if", "${"+FEATURE_META_DURABILITY_UNBREAKABLE+"}");
+		it.put("custom.title.durability.true", "Infinity");
+		it.put("custom.title.durability.false", "${"+FEATURE_META_DURABILITY+"}/${"+FEATURE_META_DURABILITY_MAX+"}");
+		it.put("custom.lore.mode.if", "${"+FEATURE_META_MODE+"}");
+		it.put("custom.lore.mode.true", "Vertical");
+		it.put("custom.lore.mode.false", "Horizonal");
+		it.put("custom.lore.blockcount.if", "${"+FEATURE_META_DURABILITY_BLOCKCOUNT+"}");
+		it.put("custom.lore.blockcount.true", " blocks");
+		it.put("custom.lore.blockcount.false", " times");
+		it.put("custom.lore.durability.if", "${"+FEATURE_META_DURABILITY_UNBREAKABLE+"}");
+		it.put("custom.lore.durability.true", "(Infinity)");
+		it.put("custom.lore.durability.false", "${"+FEATURE_META_DURABILITY+"} of ${"+FEATURE_META_DURABILITY_MAX+"}${custom.lore.blockcount}");
+		it.put("custom.lore.owner.if", "${"+FEATURE_META_OWNER+"}");
+		it.put("custom.lore.owner.true", "§3 - Owner §7: ${"+FEATURE_META_OWNER_NAME+"}${"+FEATURE_META_OWNER_ID+"}");
 
-		cfg.addDefaults(features);
+		it.put("item.title", "§eBuilder's Wand §7x${"+FEATURE_META_SIZE+"} §7[${custom.title.mode}§7] (${custom.title.durability}§7)");
+		it.put("item.lore", Lists.newArrayList(new String[] {
+				"§3 - Mode §7: ${custom.lore.mode}",
+				"§3 - Durability §7: ${custom.lore.durability}",
+				"§3 - Size §7: ${"+FEATURE_META_SIZE+"}",
+				"§3 - UseCount §7: ${"+FEATURE_META_COUNT_USE+"}",
+				"§3 - PlaceCount §7: ${"+FEATURE_META_COUNT_PLACE+"}",
+				"${custom.lore.owner}",
+		}));
 
-		cfg.addDefault(META_FORMAT, format);
-	}
-
-	private String ft(final String feature) {
-		return StringUtils.substringAfter(feature, FEATURE+".");
+		cfg.addDefaults(it);
 	}
 
 	public FileConfiguration getConfig() {
 		return this.cfg;
 	}
 
-	private ItemLoreDataFormat cachevalue;
+	public String resolve(final String str) {
+		final Pattern p = Pattern.compile("\\$\\{(.*?)\\}");
+		final Matcher m = p.matcher(str);
 
-	public ItemLoreDataFormat getFormat() {
-		if (this.cachevalue==null) {
-			final FileConfiguration config = getConfig();
-			final List<String> format = config.getStringList(META_FORMAT);
-			final ConfigurationSection featuresec = config.getConfigurationSection(FEATURE);
-			final Map<String, Object> features = featuresec.getValues(true);
-			for (final ListIterator<String> itr = format.listIterator(); itr.hasNext();) {
-				String line = itr.next();
-				for (final Entry<String, Object> entry : features.entrySet()) {
-					final String key = entry.getKey();
-					final Object value = entry.getValue();
-					if (value instanceof String)
-						line = StringUtils.replace(line, key, (String) value);
+		final StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			final String key = m.group();
+			final String value = get(key);
+			String res = null;
+			c: {
+				if (value==null) {
+					final String valueif = get(key+".if");
+					if (valueif!=null) {
+						final String resif = resolve(valueif);
+						final String valuetrue = get(key+(BooleanUtils.toBooleanObject(resif) ? ".true" : ".false"));
+						if (valuetrue!=null)
+							res = resolve(valueif);
+						else
+							res = "";
+						break c;
+					}
+				} else {
+					res = resolve(value);
+					break c;
 				}
-				itr.set(line);
 			}
-			this.cachevalue = new ItemLoreDataFormat(key(META_PREFIX), key(META_VALUE_PREFIX), key(META_VALUE_SUFFIX), format);
+			if (res!=null)
+				m.appendReplacement(sb, res);
+			else
+				m.appendReplacement(sb, "${"+key+"}");
 		}
-		return this.cachevalue;
+		m.appendTail(sb);
+
+		return sb.toString();
 	}
 
-	public String key(final String path) {
+	public String get(final String path) {
 		final Object key = getConfig().get(path);
 		if (key instanceof String) {
 			final String keystr = (String) key;
@@ -136,12 +137,5 @@ public class WandData {
 				return keystr;
 		}
 		return null;
-	}
-
-	public String keyData(final String path) {
-		final String key = key(path);
-		if (key==null)
-			return key(path+".data");
-		return key;
 	}
 }
